ce5042b194dd6f4f4ab6410d098ec76f787c2d50
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-07 11:45:58 +0530

    feat: add repository layer

---
+++ src/main/java/com/example/hms/repository/PatientRepository.java
@@ -0,0 +1,8 @@
+package com.example.hms.repository;
+
+import com.example.hms.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
