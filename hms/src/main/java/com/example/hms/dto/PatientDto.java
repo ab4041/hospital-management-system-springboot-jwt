5ac92c641de57a7b4a42a8808e216448334d81e2
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-07 11:15:14 +0530

    feat: add PatientDto class

---
+++ hms/src/main/java/com/example/hms/dto/PatientDto.java
@@ -0,0 +1,16 @@
+package com.example.hms.dto;
+
+import lombok.Data;
+
+@Data
+public class PatientDto {
+
+    private Long id;
+    private String name;
+    private int age;
+    private String gender;
+    private String disease;
+    private String address;
+    private String phoneNumber;
+
+}
